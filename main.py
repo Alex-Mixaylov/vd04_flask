b6b64387e74dfb968ff6dc63480940b2e758582d
Alex <guardamar81@gmail.com>
2024-07-20 19:06:20 +0400

    1

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+# @app.route("/")
+# @app.route("/<password>/")
+#
+# def name(password = None):
+#     if password == '1234':
+#         return f"Доступ разрешен под паролем, {password}"
+#     else:
+#         return f"Доступ зпрещен"
+
 @app.route("/")
-
-def hello_world():
-    return "Hello World!"
-
-@app.route("/new/")
-@app.route("/new_page/")
-@app.route("/новаястраница/")
-
-def new():
-    return "New page!"
-
+def render():
+    return render_template("index.html")
 if __name__ == "__main__":
     app.run()
