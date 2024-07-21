c746b58814d4bf1500a2c76f0e3816e660784007
Alex <guardamar81@gmail.com>
2024-07-21 11:09:11 +0400

    1
    1

--- dz2_vd04.py
+++ dz2_vd04.py
@@ -4,5 +4,14 @@ app = Flask(__name__)
 @app.route("/")
 def site_launch():
     return render_template("dz2_vd04.html")
-if __name__ == "__main__":
-    app.run()
+
+@app.route("/blog/")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/contacts/")
+def contacts():
+    return render_template("contacts.html")
+
+# Запуск сайта
+app.run()
